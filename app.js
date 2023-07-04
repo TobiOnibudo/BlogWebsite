43b3dab234b5cde8c4b4b79cf206ea0f55e3765b
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-07-04 18:32:39 -0300

    Added dynamic Endpoint

--- app.js
+++ app.js
@@ -54,6 +54,11 @@ app.post("/compose", function(req,res)
 
 });
 
+app.get("/posts/:postName", function(req,res)
+{
+  console.log(req.params.postName);
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
